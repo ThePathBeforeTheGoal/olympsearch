553b9561239c2dc92a66efbf040d632fad3b39d9
ThePathBeforeTheGoal <thekeeperofazeroth@gmail.com>
2025-11-27 02:54:19 +0500

    fix(db): make get_session a contextmanager

--- shared/db/engine.py
+++ shared/db/engine.py
@@ -1,21 +1,32 @@
 ﻿# shared/db/engine.py
 from sqlmodel import SQLModel, create_engine, Session
-from typing import Generator
+from typing import Generator, Iterator
 from shared.settings import settings
+from contextlib import contextmanager
 
 _engine = None
 
 def get_engine():
     global _engine
     if _engine is None:
-        # echo=False в контейнере, чтобы не спамить логи
+        # echo=False чтобы не засорять логи
         _engine = create_engine(settings.DATABASE_URL, echo=False, pool_pre_ping=True)
     return _engine
 
-def get_session() -> Generator[Session, None, None]:
+@contextmanager
+def get_session() -> Iterator[Session]:
+    """
+    Контекстный менеджер для сессий SQLAlchemy / SQLModel.
+    Использование:
+        with get_session() as session:
+            ...
+    """
     engine = get_engine()
-    with Session(engine) as session:
+    session = Session(engine)
+    try:
         yield session
+    finally:
+        session.close()
 
 def init_db():
     """
